52ec1d7fd9c640ca3a4593bf4cf2b1d2da5db2d5
koshiro564256-lgtm <koshiro564256@gmail.com>
2026-06-24 01:32:49 +0900

    Update Ui.lua

--- Ui.lua
+++ Ui.lua
@@ -603,7 +603,7 @@ function OrionLib:MakeWindow(WindowConfig)
 		Size = UDim2.new(0.5, 0, 1, 0),
 		BackgroundTransparency = 1
 	}), {
-		AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://7072719338"), {
+		AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://93680533843826"), {
 			Position = UDim2.new(0, 9, 0, 6),
 			Size = UDim2.new(0, 18, 0, 18),
 			Name = "Ico"
